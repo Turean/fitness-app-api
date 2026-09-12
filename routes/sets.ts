ce688ae07883aab29430fea7362cf2c309ae530c
Thurein Thant <thureinthant345@gmail.com>
2026-09-12 08:15:19 +0100

    update url naming covention for sets route

--- routes/sets.ts
+++ routes/sets.ts
@@ -4,54 +4,62 @@ import { auth } from "../middlewares/auth"
 
 export const router = express.Router()
 
-router.get("/:workoutExerciseId/sets", auth, async (req, res) => {
-    const workoutExerciseId = req.params?.workoutExerciseId
+router.get(
+    "/workoutExercises/:workoutExerciseId/sets",
+    auth,
+    async (req, res) => {
+        const workoutExerciseId = req.params?.workoutExerciseId
 
-    const workoutExercise = await prisma.workoutExercise.findFirst({
-        where: {
-            id: Number(workoutExerciseId),
-            session: {
-                userId: res.locals.user.id,
-            },
-        },
-        include: {
-            sets: true,
-        },
-    })
-
-    if (workoutExercise) {
-        return res.json(workoutExercise.sets)
-    }
-
-    res.status(404).json({ error: "Workout exercise not found" })
-})
-
-router.delete("/:workoutExerciseId/sets/:setId", auth, async (req, res) => {
-    const setId = req.params?.setId
-    const workoutExerciseId = req.params?.workoutExerciseId
-
-    const set = await prisma.set.findFirst({
-        where: {
-            id: Number(setId),
-            workoutExercise: {
+        const workoutExercise = await prisma.workoutExercise.findFirst({
+            where: {
                 id: Number(workoutExerciseId),
                 session: {
                     userId: res.locals.user.id,
                 },
             },
-        },
-    })
-
-    if (!set) {
-        return res.status(404).json({ msg: "Set not found" })
-    }
-
-    try {
-        const deleteSet = await prisma.set.delete({
-            where: { id: set.id },
+            include: {
+                sets: true,
+            },
         })
-        return res.status(200).json(deleteSet)
-    } catch (e) {
-        return res.status(400).json({ msg: "failed to delete the set" })
-    }
-})
+
+        if (workoutExercise) {
+            return res.json(workoutExercise.sets)
+        }
+
+        res.status(404).json({ error: "Workout exercise not found" })
+    },
+)
+
+router.delete(
+    "/workoutExercises/:workoutExerciseId/sets/:setId",
+    auth,
+    async (req, res) => {
+        const setId = req.params?.setId
+        const workoutExerciseId = req.params?.workoutExerciseId
+
+        const set = await prisma.set.findFirst({
+            where: {
+                id: Number(setId),
+                workoutExerciseId: Number(workoutExerciseId),
+                workoutExercise: {
+                    session: {
+                        userId: res.locals.user.id,
+                    },
+                },
+            },
+        })
+
+        if (!set) {
+            return res.status(404).json({ msg: "Set not found" })
+        }
+
+        try {
+            const deleteSet = await prisma.set.delete({
+                where: { id: set.id },
+            })
+            return res.status(200).json(deleteSet)
+        } catch (e) {
+            return res.status(400).json({ msg: "failed to delete the set" })
+        }
+    },
+)
